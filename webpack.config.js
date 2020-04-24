e1b5f1930ad6c3352bcfd28e39252c0fa1395ce3
Vasya231 <gg231589@gmail.com>
2020-04-24 13:47:29 +0300

    build: install and configure html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/src/index.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS aggregator',
+    }),
+  ],
+};
