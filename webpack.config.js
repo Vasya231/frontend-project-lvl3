04ca76b4c9ce11b4547e19017cefc274dd956950
Vasya231 <gg231589@gmail.com>
2020-04-24 15:02:47 +0300

    build: install babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: `${__dirname}/src/index.js`,
   output: {
     path: `${__dirname}/dist`,
     filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
