8d313af6392684c337ac40288c9df565f099406c
Vasya231 <gg231589@gmail.com>
2020-05-16 20:31:24 +0300

    refactor: rename initView -> initApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './scss/app.scss';
-import initView from './view';
+import initApp from './view';
 import State from './State';
 import { generateInputHandler, generateSubmitHandler, restartTimer } from './controller';
 
 const state = new State();
-initView(state, generateSubmitHandler(state), generateInputHandler(state));
+initApp(state, generateSubmitHandler(state), generateInputHandler(state));
 restartTimer(state);
