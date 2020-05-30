d64395f5e611608495e98ccaf4c0c1b56efb0702
Vasya231 <gg231589@gmail.com>
2020-05-30 16:30:17 +0300

    refactor: merge controller.js with app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,103 @@
 import i18 from 'i18next';
+import axios from 'axios';
+import { uniqueId } from 'lodash';
 import texts from './locales';
 import init from './view';
-import { generateInputHandler, generateSubmitHandler } from './controller';
+import { parseRss, isValidUrl, proxifyUrl } from './utils';
+import getErrorType from './errors';
+import settings from './settings';
+
+const loadRss = (rssLink) => axios
+  .get(proxifyUrl(rssLink), { timeout: settings.responseTimeout })
+  .then((response) => {
+    const isRss = response.headers['content-type'].includes('application/rss+xml');
+    if (!isRss) {
+      throw new Error('notRss');
+    }
+    const parsedObj = parseRss(response.data);
+    return Promise.resolve(parsedObj);
+  });
+
+const addItemToFeed = (state, feedId, {
+  title, description, link, pubDate,
+}) => {
+  const dateAdded = pubDate || Date.now();
+  state.posts.push({
+    feedId, title, description, link, dateAdded,
+  });
+};
+
+const updateFeed = (state, feedId) => {
+  const feed = state.feeds.find(({ id }) => (feedId === id));
+  const { link } = feed;
+  loadRss(link).then((parsedRss) => {
+    const {
+      title, description, items,
+    } = parsedRss;
+    feed.link = link;
+    feed.title = title;
+    feed.description = description;
+    const oldItems = state.posts.filter(({ feedId: currentFeedId }) => (feedId === currentFeedId));
+    const feedHasItem = (item) => (
+      oldItems.findIndex(({ link: currentLink }) => (currentLink === item.link)) !== -1
+    );
+    const newItems = items.filter((item) => !(feedHasItem(item)));
+    newItems.forEach((item) => addItemToFeed(state, feedId, item));
+  }).catch((e) => {
+    console.log(`ERROR while updating feed ${link}:`, e);
+  }).then(() => setTimeout(() => updateFeed(state, feedId), settings.refreshTimeout));
+};
+
+const generateSubmitHandler = (state) => (event) => {
+  event.preventDefault();
+  const formValue = state.form.value;
+  if (!isValidUrl(formValue)) {
+    state.form.error = 'notUrl';
+    return;
+  }
+  const rssLink = new URL(formValue).href;
+  const { feeds } = state;
+  if (feeds.findIndex(({ link }) => (link === rssLink)) !== -1) {
+    state.form.error = 'alreadyAdded';
+    return;
+  }
+  state.form.processState = 'sending';
+  loadRss(rssLink)
+    .then((parsedRss) => {
+      const {
+        title, description, items,
+      } = parsedRss;
+      const id = uniqueId();
+      const newFeed = {
+        id, link: rssLink, title, description, items: [],
+      };
+      state.feeds.push(newFeed);
+      items.forEach((item) => addItemToFeed(state, id, item));
+      state.form.value = '';
+      state.form.error = '';
+      state.form.processState = 'filling';
+      state.form.valid = true;
+      setTimeout(() => updateFeed(state, id), settings.refreshTimeout);
+    })
+    .catch((error) => {
+      state.form.processState = 'filling';
+      const errorType = getErrorType(error);
+      state.form.error = errorType;
+      if (errorType === 'unknownError') {
+        console.log('Unexpected error occured:');
+        console.log(error);
+      }
+    });
+};
+
+const generateInputHandler = (state) => (event) => {
+  event.preventDefault();
+  const form = event.target.closest('form.rss-form');
+  const formData = new FormData(form);
+  const value = formData.get('url');
+  state.form.value = value;
+  state.form.valid = isValidUrl(value) || (value === '');
+};
 
 export default () => {
   const state = {
